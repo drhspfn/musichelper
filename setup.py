4a2e12bd00de581932bd8f3f657d885fc7816562
drhspfn <jenya.gsta@gmail.com>
2024-06-12 22:34:37 +0300

    setup update

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='music-helper',
-    version='2.0.4',
+    name='musichelper',
+    version='3.0.0b',
     description='Asynchronous library for working with streaming services. Search, download',
     author='drhspfn',
     author_email="jenya.gsta@gmail.com",
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/drhspfn/music-helper",
+    url="https://github.com/drhspfn/musichelper",
     install_requires=[
         "aiofiles",
         "soundcloud-v2",
@@ -22,12 +22,10 @@ setuptools.setup(
         "pytube",
         "ytmusicapi",
         "httpx[http2]",
-        "yarl",
-        "asyncio"
+        "yarl"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
     ],
-
 )
